fa321e43e332ba476ed1bc1ae4abb2855b40391b
daniel-macias <josedanielmr@gmail.com>
2022-11-03 23:51:42 -0600

    first commit and react setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Landmass Generator</h1>
+        <p>Testing credential 5</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
